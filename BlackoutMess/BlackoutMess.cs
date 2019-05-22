87d957c1c9e94d4d50d7f982b56ac337ee8e6948
Hammak <saint3543jakuzi@gmail.com>
2019-05-22 11:34:04 +0300

    Added command loop

--- BlackoutMess/BlackoutMess.cs
+++ BlackoutMess/BlackoutMess.cs
@@ -5,12 +5,14 @@ namespace BlackoutMess
 {
     internal static class BlackoutMess
     {
+        private static bool shouldExit = false;
+
         public static void Main(string[] args)
         {
             var dbHelper = new DBHelper();
             var settings = new Settings();
             var chatDataManager = new ChatDataManager(dbHelper, settings);
-            
+
             int result;
             do
             {
@@ -22,10 +24,21 @@ namespace BlackoutMess
 
                 result = chatDataManager.AuthUser(userName, password);
             } while (result != 0);
-            
+
             chatDataManager.PrintAvailableChats();
-            
+
             Printer.PrintHelp();
+
+            do
+            {
+                string input = Console.ReadLine();
+
+                if (input != null && input.StartsWith("/"))
+                {
+                    string[] decomposedInput = input.Split();
+
+                }
+            } while (!shouldExit);
         }
     }
 }
