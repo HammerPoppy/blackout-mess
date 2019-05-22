0da54815e16032f55c43b36739dd7d39db61a798
Hammak <saint3543jakuzi@gmail.com>
2019-05-22 09:18:42 +0300

    Initialized core objects

---
+++ BlackoutMess/BlackoutMess.cs
@@ -0,0 +1,14 @@
+﻿namespace BlackoutMess
+{
+    internal static class BlackoutMess
+    {
+        public static void Main(string[] args)
+        {
+            var dbHelper = new DBHelper();
+            var settings = new Settings();
+            var chatDataManager = new ChatDataManager(dbHelper, settings);
+            
+            
+        }
+    }
+}
